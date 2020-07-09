2bb3cda3b08ad91dd80adf06b5a679ed5f8d0ad8
JinHsu <janice_95@126.com>
2020-07-09 10:35:08 +0800

    首页

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+// 西瓜打开
+
+$(function(){
+    $('#watermelon').click(function () {
+        $("#img_gua").hide();
+        $('#img_gua_left').show().fadeOut("slow","linear");
+        $('#img_gua_right').show().fadeOut("slow","linear");
+    });
+})
